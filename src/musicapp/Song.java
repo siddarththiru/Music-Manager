92ea2343726adb2d8adb03760f497438078963cc
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-03-14 13:22:28 +0000

    created song.java class

---
+++ src/musicapp/Song.java
@@ -0,0 +1,12 @@
+package musicapp;
+public class Song {
+    //defining data members a song object will have
+    private String name;
+    private String genre;
+    
+    //constructor used to create a Song object
+    public Song(String name, String genre){
+        this.name = name;
+        this.genre = genre;
+    }
+}
