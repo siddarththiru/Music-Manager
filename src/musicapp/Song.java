df2c086c35c670fc5436f9367debd074126b41df
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-03-14 20:05:18 +0000

    added access methods for data members for Song Class

--- src/musicapp/Song.java
+++ src/musicapp/Song.java
@@ -9,4 +9,30 @@ public class Song {
         this.name = name;
         this.genre = genre;
     }
+    public Song(String name){
+        this.name = name;
+        genre = "";
+    }
+    public Song(){
+        name = "";
+        genre = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    
 }
